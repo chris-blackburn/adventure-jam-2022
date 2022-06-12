4907fb41a0a752cd9a1004dafcbab4d113e7c697
TJ Koonce <tj7@vt.edu>
2022-06-12 19:00:38 -0400

    updated menus

--- ActionRPG/UI/PauseMenu.gd
+++ ActionRPG/UI/PauseMenu.gd
@@ -1,10 +1,18 @@
 extends Control
 
+onready var resumeBtn = $PauseContainer/VBoxContainer/ResumeBtn
+onready var masterSlider = $SoundsContainer/VBoxContainer/Master
+onready var mainMenuBtn = $RestartContainer/VBoxContainer/HBoxContainer/Yes
+
 var is_paused = false setget set_is_paused
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("pause"):
 		self.is_paused = !is_paused
+		$PauseContainer.visible = true
+		$SoundsContainer.visible = false
+		$RestartContainer.visible = false
+		resumeBtn.grab_focus()
 
 func set_is_paused(value):
 	is_paused = value
@@ -14,6 +22,25 @@ func set_is_paused(value):
 func _on_ResumeBtn_pressed() -> void:
 	self.is_paused = false
 
+func _on_SoundsBtn_pressed() -> void:
+	$PauseContainer.visible = !$PauseContainer.visible
+	$SoundsContainer.visible = !$SoundsContainer.visible
+	masterSlider.grab_focus()
 
-func _on_QuitBtn_pressed() -> void:
-	get_tree().quit()
+func _on_MainMenuBtn_pressed() -> void:
+	$PauseContainer.visible = !$PauseContainer.visible
+	$RestartContainer.visible = !$RestartContainer.visible
+	mainMenuBtn.grab_focus()
+
+func _on_Back_pressed() -> void:
+	$PauseContainer.visible = !$PauseContainer.visible
+	$SoundsContainer.visible = !$SoundsContainer.visible
+	resumeBtn.grab_focus()
+
+func _on_Yes_pressed() -> void:
+	get_tree().change_scene("res://UI/StartMenu.tscn")
+
+func _on_No_pressed() -> void:
+	$PauseContainer.visible = !$PauseContainer.visible
+	$RestartContainer.visible = !$RestartContainer.visible
+	resumeBtn.grab_focus()
--- ActionRPG/UI/StartMenu.gd
+++ ActionRPG/UI/StartMenu.gd
@@ -1,20 +1,32 @@
 extends Control
 
-onready var startButton = $VBoxContainer/StartButtonStartButton
+onready var startButton = $VBoxContainer/StartButton
 onready var optionsButton = $VBoxContainer/OptionsButton
 onready var quitButton = $VBoxContainer/QuitButton
+onready var masterSlider = $SoundsContainer/VBoxContainer/Master
 
 func _ready():
-	$VBoxContainer/StartButton.grab_focus()
-	
+	startButton.grab_focus()
 
 func _on_StartButton_pressed():
 	get_tree().change_scene("res://World.tscn")
-
 
 func _on_QuitButton_pressed():
 	get_tree().quit()
 
 
+func _on_SoundsButton_pressed() -> void:
+	$VBoxContainer.visible = !$VBoxContainer.visible
+	$SoundsContainer.visible = !$SoundsContainer.visible
+	masterSlider.grab_focus()
+
+
+func _on_Back_pressed() -> void:
+	$VBoxContainer.visible = !$VBoxContainer.visible
+	$SoundsContainer.visible = !$SoundsContainer.visible
+	startButton.grab_focus()
+
+
 func _on_OptionsButton_pressed():
 	pass # Replace with function body.
+
