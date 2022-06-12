37c52f2548ab45e8d1da15d319bb286d84421c1c
TJ Koonce <tj7@vt.edu>
2022-06-12 15:45:45 -0400

    Pause menu added

---
+++ ActionRPG/UI/PauseMenu.gd
@@ -0,0 +1,19 @@
+extends Control
+
+var is_paused = false setget set_is_paused
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("pause"):
+		self.is_paused = !is_paused
+
+func set_is_paused(value):
+	is_paused = value
+	get_tree().paused = is_paused
+	visible = is_paused
+
+func _on_ResumeBtn_pressed() -> void:
+	self.is_paused = false
+
+
+func _on_QuitBtn_pressed() -> void:
+	get_tree().quit()
