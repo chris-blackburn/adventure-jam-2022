460840b9d85025d3ccabccf67f40682c78ae4a35
Austin <austin98@vt.edu>
2022-06-13 14:34:53 -0700

    name changes

--- ActionRPG/UI/StartMenu.gd
+++ ActionRPG/UI/StartMenu.gd
@@ -3,13 +3,13 @@ extends Control
 onready var startButton = $VBoxContainer/StartButtonStartButton
 onready var optionsButton = $VBoxContainer/OptionsButton
 onready var quitButton = $VBoxContainer/QuitButton
-onready var audioStreamPlayer = $AudioStreamPlayer
+onready var backgroundMusic = $BackgroundMusic
 
 var stats = PlayerStats
 
 func _ready():
 	$VBoxContainer/StartButton.grab_focus()
-	audioStreamPlayer.play()
+	backgroundMusic.play()
 	
 
 func _on_StartButton_pressed():
