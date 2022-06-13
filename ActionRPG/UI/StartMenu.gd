de9fa8f588ebd9030bc9266e0de1e1048793dd3a
Austin <austin98@vt.edu>
2022-06-13 14:28:57 -0700

    music added to start menu

--- ActionRPG/UI/StartMenu.gd
+++ ActionRPG/UI/StartMenu.gd
@@ -3,15 +3,18 @@ extends Control
 onready var startButton = $VBoxContainer/StartButtonStartButton
 onready var optionsButton = $VBoxContainer/OptionsButton
 onready var quitButton = $VBoxContainer/QuitButton
+onready var audioStreamPlayer = $AudioStreamPlayer
 
 var stats = PlayerStats
 
 func _ready():
 	$VBoxContainer/StartButton.grab_focus()
+	audioStreamPlayer.play()
 	
 
 func _on_StartButton_pressed():
 	stats.set_health(stats.get_max_health())
+	# warning-ignore:return_value_discarded
 	get_tree().change_scene("res://World.tscn")
 
 
